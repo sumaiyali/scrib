659e5672b6ce05345be43e56737c724ac2ccca71
sumaiyali <113270396+sumaiyali@users.noreply.github.com>
2023-12-22 13:39:00 -0500

    Add files via upload

--- app1.js
+++ app1.js
@@ -1,79 +1,113 @@
-var express = require('express')
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-var imgSchema = require('./model.js');
-var fs = require('fs');
-var path = require('path');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var imgSchema = require("./model.js");
+const collection = require("./mongo");
+var fs = require("fs");
+var path = require("path");
+var cors = require("cors");
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
-require('dotenv').config();
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(console.log("DB Connected"))
+mongoose.connect(process.env.MONGO_URL).then(console.log("DB Connected"));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-
-
-var multer = require('multer');
+var multer = require("multer");
 
 var storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'uploads')
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.fieldname + '-' + Date.now())
-	}
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
 });
-
 
 var upload = multer({ storage: storage });
 
-app.get('/', (req, res) => {
-	imgSchema.find({})
-	.then((data, err)=>{
-		if(err){
-			console.log(err);
-		}
-		res.render('imagepage',{items: data})
-	})
+app.get("/", cors(), (req, res) => {});
+
+app.get("/image", async (req, res) => {
+  imgSchema.find().then((data, err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.json({ data });
+  });
 });
 
-
-app.post('/', upload.single('image'), (req, res, next) => {
-
-	var obj = {
-		name: req.body.name,
-		desc: req.body.desc,
-		img: {
-			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-			contentType: 'image/png'
-		}
-	}
-	imgSchema.create(obj)
-	.then ((err, item) => {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			// item.save();
-			res.redirect('/');
-		}
-	});
+app.post("/image", upload.single("image"), (req, res, next) => {
+  var obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  imgSchema.create(obj).then((err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect("/");
+    }
+  });
 });
 
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const check = await collection.findOne({
+      email: email,
+      password: password,
+    });
 
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+    }
+  } catch (e) {
+    res.json("fail");
+  }
+});
 
+app.post("/signup", async (req, res) => {
+  const { email, password, username } = req.body;
 
+  const data = {
+    email: email,
+    password: password,
+    username: username,
+  };
 
-var port = process.env.PORT || 3003;
-app.listen(port, err => {
-	if (err)
-		throw err
-	console.log('Server listening on port', port)
-})
+  try {
+    const check = await collection.findOne({ email: email });
+
+    if (check) {
+      res.json("exist");
+    } else {
+      res.json("notexist");
+      await collection.insertMany([data]);
+    }
+  } catch (e) {
+    res.json("fail");
+  }
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server listening on port", port);
+});
